f577d108129786876cfbc840527f6c124bdbc33c
sejalpalo <sejalpalo456@gmail.com>
2024-03-25 10:01:19 +0530

    3rd commit

---
+++ Framework123/src/main/java/Pratice/ToCreateLead.java
@@ -0,0 +1,9 @@
+package Pratice;
+
+public class ToCreateLead {
+
+	public static void main(String[] args) {
+    System.out.println("created Lead Sucssfuly");
+	}
+
+}
--- Framework123/src/main/java/Pratice/ToCreateOrg.java
+++
@@ -1,9 +0,0 @@
-package Pratice;
-
-public class ToCreateOrg {
-
-	public static void main(String[] args) {
-    System.out.println("Org create succesfully");
-	}
-
-}
