d527ac54f91b208e47e59b63b95356a0f639bcbc
sejalpalo <sejalpalo456@gmail.com>
2024-03-25 10:36:33 +0530

    5 commit

---
+++ Framework123/src/main/java/Pratice/ToCreate.java
@@ -0,0 +1,9 @@
+package Pratice;
+
+public class ToCreate {
+
+	public static void main(String[] args) {
+    System.out.println("i am sejal");
+	}
+
+}
--- Framework123/src/main/java/Pratice/ToCreateLead.java
+++
@@ -1,9 +0,0 @@
-package Pratice;
-
-public class ToCreateLead {
-
-	public static void main(String[] args) {
-    System.out.println("created Lead Sucssfuly");
-	}
-
-}
